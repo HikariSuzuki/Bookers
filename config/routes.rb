c02b631d7995d0e09e22e381b3fcdc1d862160f8
HikariSuzuki <hikari.suzuki1296@gmail.com>
2021-05-03 06:51:27 +0000

    最初のコミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'books/def'
+  get 'books/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get 'index' => 'books#index',as: 'index'
+  get 'new' => 'books#new',as: 'new'
+  get 'edit' => 'books#edit',as: 'edit'
+  get 'show' => 'books#show',as: 'show'
+  post 'new' => 'books#new'
+  post 'index' => 'books#index'
+  delete 'index/:id' => 'index#destroy'
+  get 'books/:id' => 'books#show', as:'book'
+  get 'books/:id/edit' => 'books#edit', as:'edit_book'
+  patch 'books/:id/edit' => 'books#update',as:'update_book'
+  delete 'books/:id' => 'books#destroy', as:'destroy_book'
 end
--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get def" do
+    get books_def_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get books_new_url
+    assert_response :success
+  end
+
 end
