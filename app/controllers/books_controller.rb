ec035d950413fcd8c4a40e90e1c8b50ee8cb7eab
HikariSuzuki <hikari.suzuki1296@gmail.com>
2021-05-05 09:11:15 +0000

    エラーメッセージ、フラッシュメッセージの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,4 @@
 class BooksController < ApplicationController
-
 def index
    @books=Book.all
    @book=Book.new
@@ -14,32 +13,43 @@ def new
 end
 
 def create
-  book=Book.new(book_params)
+    book=Book.new(book_params)
   book.save
-  redirect_to books_path
-end
-
-def edit
-    @book=Book.find(params[:id])
-end
-
-def update
-    book=Book.find(params{:id})
-    book.update(book_params)
+  
+ if book.save
+    flash[:notice] = "Book was successfully created."
     redirect_to book_path(book.id)
+ else
+    render books_path
+ end
 end
+  
+ def edit
+    @book=Book.find(params[:id])
+ end
 
-def destroy
+ def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+  if book.save
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(book.id)
+  end
+ end
+   
+
+ def destroy
     book=Book.find(params[:id])
     book.destroy
-    redirect_to book_path
-end
+  if book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to book_path(book.id)
+  end
+ end
 
 private
-def book_params
+ def book_params
   params.require(:book).permit(:title,:body)
-end
-
-
+ end
 
 end
